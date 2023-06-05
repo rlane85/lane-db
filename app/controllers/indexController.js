c4aa4139cd41121eaa069dfb7bbd77863e6f6f7f
Bob Lane <blane2245@gmail.com>
2023-06-05 14:13:45 +0000

    reconstitute

--- app/controllers/indexController.js
+++ app/controllers/indexController.js
@@ -3,6 +3,7 @@ const clientSecret = "clientSecret";
 exports.index = function(req, res, next) {
     const ID = req.query.clientID;
     const secret = req.query.clientSecret;
+    console.log("checking authentication", JSON.stringify(req.query));
     if (ID == "clientID" && secret == "clientSecret") {
         res.json({token: "good"});
     } else {
